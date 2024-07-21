18076a9d60967df14926f69a6806e7daaf7dc440
lderuijter <85785995+lderuijter@users.noreply.github.com>
2024-07-21 19:56:30 +0200

    added new files, trying to connect vue with laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import Welcome from './components/Welcome.vue';
+
+const app = createApp({})
+app.component('Welcome', Welcome)
+app.mount('#app')
---
+++ resources/views/test.blade.php
@@ -0,0 +1,8 @@
+<head>
+  <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}" />
+  <script defer src="{{ mix('resources/js/app.js') }}"></script>
+</head>
+
+<div id="app">
+  <Welcome />
+</div>
