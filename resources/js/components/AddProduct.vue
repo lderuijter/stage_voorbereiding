39495eea0510b7a34b38c76875217d9b106e3b28
lderuijter <85785995+lderuijter@users.noreply.github.com>
2024-08-02 16:08:41 +0200

    added button to go back to all products and form clears after submitting

--- resources/js/components/AddProduct.vue
+++ resources/js/components/AddProduct.vue
@@ -3,35 +3,41 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h4>Add Product</h4>
+                    <h1>Add Product</h1>
                 </div>
                 <div class="card-body">
-                    <form @submit.prevent="create">
+                    <form @submit.prevent="create" id="createProduct">
                         <div class="row">
                             <div class="col-12 mb-2">
                                 <div class="form-group">
                                     <label>Title</label>
-                                    <input type="text" class="form-control" v-model="product.title">
+                                    <input id="title" type="text" class="form-control" v-model="product.title">
                                 </div>
                             </div>
                             <div class="col-12 mb-2">
                                 <div class="form-group">
                                     <label>Description</label>
-                                    <input type="text" class="form-control" v-model="product.description">
+                                    <input id="description" type="text" class="form-control" v-model="product.description">
                                 </div>
                             </div>
                             <div class="col-12 mb-2">
                                 <div class="form-group">
                                     <label>Is this product certified?</label>
                                     <input
+                                        id="certified"
                                         type="checkbox"
                                         @input="checked = $event.target.checked"
                                         v-model="product.is_certified"
                                     />
                                 </div>
                             </div>
+                            <br>
                             <div class="col-12">
                                 <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                            <br>
+                            <div class="col-12">
+                                <a href="/">Back to all products</a>
                             </div>
                         </div>                        
                     </form>
@@ -57,6 +63,9 @@ export default {
     methods:{
         async create(){
             await axios.post('/api/product', this.product).then(response=>{
+                const createProductForm = document.getElementById('createProduct')
+                createProductForm.submit()
+                createProductForm.reset()
                 console.log(response)
             }).catch(error=>{
                 console.log(error)
--- resources/js/components/ProductIndex.vue
+++ resources/js/components/ProductIndex.vue
@@ -1,3 +1,16 @@
+<template>
+  <a href="/create">Create Product</a>
+  <h1>All Products</h1>
+  <div>
+    <div v-for="product in products" :key="product.id">
+      ---
+      <h2>Title: {{ product.title }}</h2>
+      <h2>Description: {{ product.description }}</h2>
+      <h3 v-if="product.is_certified">Product is certified</h3>
+    </div>
+  </div>
+</template>
+
 <script setup>
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
@@ -11,17 +24,3 @@ const getProducts = () => {
 
 onMounted(() => getProducts())
 </script>
-
-<template>
-  <a href="/create">Create Product</a>
-  <h1>All Products</h1>
-  <div>
-    <ul>
-      <li v-for="product in products" :key="product.id">
-        <h2>Title: {{ product.title }}</h2>
-        <h2>Description: {{ product.description }}</h2>
-        <h3 v-if="product.is_certified">Product is certified</h3>
-      </li>
-    </ul>
-  </div>
-</template>
